1936333a7bb81182402fc1c2421366df7f01451b
Tim Riley <tim@openmonkey.com>
2015-11-10 13:54:13 +1100

    Move container auto-loaded files away from lib/ into app/
    This lets us keep lib/ for regular, explicitly-required internal library files.

--- core/app_prototype/container.rb
+++ core/app_prototype/container.rb
@@ -5,8 +5,14 @@ module AppPrototype
     setting :root, Pathname(__FILE__).join("../..").realpath.dirname.freeze
     setting :auto_load, true
 
+    $LOAD_PATH.unshift(root.join("app").to_s)
     $LOAD_PATH.unshift(root.join("lib").to_s)
     $LOAD_PATH.unshift(root.join("core").to_s)
     $LOAD_PATH.unshift(root.join("vendor").to_s)
+
+    # Set default auto-load path to app/
+    def self.lib_path
+      root.join("app")
+    end
   end
 end
