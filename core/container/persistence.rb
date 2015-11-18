e249750b23c48b5f6f1412412567fe1f11c8074b
Tim Riley <tim@openmonkey.com>
2015-11-18 15:57:47 +1100

    Tidy syntax

--- core/container/persistence.rb
+++ core/container/persistence.rb
@@ -1,15 +1,15 @@
 AppPrototype::Container.namespace "persistence" do |container|
   ## Register persistence operations here, e.g.
   #
-  # container.register("persist_user") do
+  # container.register "persist_user" do
   #   container["persistence.rom"].command(:users)[:create]
   # end
   #
-  # container.register("persist_post") do
+  # container.register "persist_post" do
   #   container["persistence.rom"].command(:posts)[:create]
   # end
   #
-  # container.register("update_post") do
+  # container.register "update_post" do
   #   container["persistence.rom"].command(:posts)[:update]
   # end
 end
