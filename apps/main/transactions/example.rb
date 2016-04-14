8aafa23341a6eb589023c5ace13bb7c63760baf4
Tim Riley <tim@openmonkey.com>
2016-04-14 16:01:12 +1000

    Use dry-transaction, via a central transaction definition object

--- apps/main/transactions/example.rb
+++ apps/main/transactions/example.rb
@@ -1,8 +1,7 @@
 Main::Transactions.define do |t|
-  # Define your Call Sheet transactions here:
+  # Define your dry-transaction objects here:
   #
-  # t.define "transactions.users.sign_up" do
-  #   step :validate, with: "operations.users.validate_sign_up"
-  #   map :persist, with: "persistence.create_user"
+  # t.define "main.transactions.users.sign_up" do
+  #   step :persist, with: "main.users.operations.sign_up"
   # end
 end
