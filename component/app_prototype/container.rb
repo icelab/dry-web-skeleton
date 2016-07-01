7ae0feae8b7af59921c922497de92cc7e837093f
Dylan Wolff <dylan@dylanwolff.com>
2016-07-01 11:40:06 +0800

    Rename root /core directory

--- component/app_prototype/container.rb
+++ component/app_prototype/container.rb
@@ -1,7 +1,8 @@
 require "dry/web/container"
+require "dry/web/umbrella"
 
 module AppPrototype
-  class Container < Dry::Web::Container
+  class Container < Dry::Web::Umbrella
     configure do
       config.name = :main
       config.auto_register = %w[lib/authentication]
