02197ce71cb6f089c9d140e5f55b49cc2c443328
Dylan Wolff <dylan@dylanwolff.com>
2016-07-01 13:14:02 +0800

    Configure settings

--- component/app_prototype/settings.rb
+++ component/app_prototype/settings.rb
@@ -11,12 +11,12 @@ module AppPrototype
       end
     end
 
-    setting :database_url, RequiredString
-    setting :session_secret, RequiredString
+    setting :database_url, Types::Required::String
+    setting :session_secret, Types::Required::String
 
-    setting :assets_server_url, Types::String
-    setting :precompiled_assets, Types::Form::Bool # TODO: add .default(false) when dry-types allows it
-    setting :precompiled_assets_host, Types::String
+    setting :assets_server_url, Types::Required::String
+    setting :precompiled_assets, Types::Form::Bool.default(false)
+    setting :precompiled_assets_host
 
     setting :app_mailer_from_email, Types::String
 
--- component/boot/config.rb
+++
@@ -1,4 +0,0 @@
-AppPrototype::Container.finalize(:config) do |container|
-  require "app_prototype/config"
-  container.register "config", AppPrototype::Config.load(container.root, "application", container.config.env)
-end
